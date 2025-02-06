51edcb6a61202bb1191131a2729f555c3c5a9a28
Kodak10 <atchinaymard10@gmail.com>
2025-02-06 19:52:31 +0000

    first commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title')</title>
+    <title>Open Bamekangoa | CHELSEA TAEKWONDO CLUB</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
     
